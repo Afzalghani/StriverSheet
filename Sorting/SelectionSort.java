6d255af2286c7e76181ec17c784d269dbf9a5eb7
Afzal <afzal.ghani@hexaviewtech.com>
2023-06-04 11:02:14 +0530

    adding the selection and bubble sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -5,41 +5,41 @@ import java.util.Scanner;
 
 public class SelectionSort {
 
-   static  int[] SelectionSort(int arr[]){
-    
-        int n= arr.length;
-
-        for(int i=0;i<n;i++){
-            for(int j= i+1;j<n;j++){
-                if(arr[i]>arr[j]){
-                   swap(arr, i, j);
-                }
-            }
-        }
-
-
-    
-        return arr;
-    
-    }
-
-   static void swap(int arr[], int i , int j){
-        int temp= arr[i];
-        arr[i]= arr[j];
+    static void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
         arr[j]= temp;
     }
-    public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-        int n= input.nextInt();
-        int arr[]=new int[n];
+    static int[] selectionSort(int[] arr){
+
+        int n= arr.length;
         for(int i=0;i<n;i++){
-            arr[i]=input.nextInt();
-        }
+            int min = i;
+            for(int j=i+1;j<n;j++){
+                if(arr[min]>arr[j]){
+                    min=j;
+                }
+            }
 
-       System.out.println(Arrays.toString(SelectionSort(arr)));
-        
+            swap(arr,i,min);
+
+        }
+           
+        return arr;
+    }
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int n= input.nextInt();
+    int arr[]= new int[n];
+
+    for(int i=0;i<n;i++){
+        arr[i]= input.nextInt();
     }
 
+    System.out.println(Arrays.toString(selectionSort(arr)));
+
+    
+}
     
 }
