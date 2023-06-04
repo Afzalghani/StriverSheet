79d3d88561cc9b9fb6b3d2322bd0a896abd07881
Afzal <afzal.ghani@hexaviewtech.com>
2023-06-04 18:04:25 +0530

    Insertion sort Implementation

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,11 +1,31 @@
 package Sorting;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InsertionSort {
 
     static int[] insertionSort(int arr[]){
+       
+        int n= arr.length;
+        for(int i=0;i<n-1;i++){
+           
 
+            int j= i+1;
+
+            while(j>0 && arr[j]<arr[j-1]){
+                int temp= arr[j];
+                arr[j]= arr[j-1];
+                arr[j-1]= temp;
+                j--;
+
+            
+                
+
+            }
+
+
+        }
 
         return arr;
     }
@@ -18,6 +38,8 @@ public class InsertionSort {
         for(int i=0;i<n;i++){
             arr[i]= input.nextInt();
         }
+
+        System.out.println(Arrays.toString(insertionSort(arr)));
         
     }
 
