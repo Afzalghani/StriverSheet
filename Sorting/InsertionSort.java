a8a93d4b376dc76d43166d68d30701993b2bd683
Afzal <afzal.ghani@hexaviewtech.com>
2023-06-04 12:34:39 +0530

    added Array Section

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,25 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+
+    static int[] insertionSort(int arr[]){
+
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n=input.nextInt();
+        int arr[]= new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]= input.nextInt();
+        }
+        
+    }
+
+    
+}
